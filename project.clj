98309a9ab5baa6614153cead59e9171d2177c01f
Micha Niskin <asdf@ubergibson.com>
2012-11-07 19:23:58 -0500

    init

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hlisp-jquery "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/micha/hlisp-jquery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :manifest {"hlisp-provides" "jquery"}
