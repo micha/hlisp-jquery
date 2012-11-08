d9554e73abb50d9c94130580276b2674acd093a6
Micha Niskin <asdf@ubergibson.com>
2012-11-07 19:22:02 -0500

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hlisp-jquery "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :manifest {"hlisp-provides" "jquery"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/hlisp_jquery/core_test.clj
@@ -0,0 +1,7 @@
+(ns hlisp-jquery.core-test
+  (:use clojure.test
+        hlisp-jquery.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
